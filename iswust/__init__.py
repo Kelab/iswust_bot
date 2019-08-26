ee62c537a68d4319007440aca3f6baaaa37e819b
Artin <lengthmin@gmail.com>
2019-08-26 21:26:15 +0800

    Remove DB

--- iswust/__init__.py
+++ iswust/__init__.py
@@ -2,16 +2,12 @@ from os import path
 from typing import Any
 
 import nonebot as nb
-from . import db
 
 
 def init(config_object: Any) -> nb.NoneBot:
 
     nb.init(config_object)
     bot = nb.get_bot()
-
-    # 数据库连接
-    bot.server_app.before_serving(db.init)
 
     nb.load_builtin_plugins()
     nb.load_plugins(path.join(path.dirname(__file__), 'plugins'),
