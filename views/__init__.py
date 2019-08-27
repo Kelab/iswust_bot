440aa9c64f69d48accd4b58f27b30238d4e9f3dc
Artin <lengthmin@gmail.com>
2019-08-27 21:11:56 +0800

    feat: 推送 api

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,6 @@
 from quart import Blueprint
 
-api = Blueprint('api', __name__, url_prefix='/api/v1')
+api = Blueprint('api', __name__, url_prefix='/api')
 
 from . import push
 # from . import admin
--- views/push.py
+++ views/push.py
@@ -2,7 +2,9 @@ import nonebot as nb
 from . import api
 from quart import request, jsonify
 from nonebot import CQHttpError
+from utils.tools import xor_decrypt, check_args
 from typing import Optional
+from log import IS_LOGGER
 bot = nb.get_bot()
 
 
@@ -11,15 +13,32 @@ async def push():
     query: dict = request.args
     qq_: Optional[str] = query.get('qq')
     msg_: Optional[str] = query.get('msg')
+    verifycode_: Optional[str] = query.get('verifycode')
+    decrypt_qq: Optional[int] = None
+
+    result, msg = check_args(qq=qq_, msg=msg_, verifycode=verifycode_)
+
     rcode_ = 403
-    rmsg_ = ""
-    try:
-        if qq_ and msg_:
-            await bot.send_private_msg(user_id=qq_, message=msg_)
-            rcode_ = 200
-            rmsg_ = "发送成功"
-    except CQHttpError:
-        rcode_ = 500
-        rmsg_ = "发送失败"
-    bot._server_app.config['JSONIFY_MIMETYPE'] = "text/html"
+    rmsg_ = msg
+
+    if result:
+        if verifycode_:
+            decrypt_qq = xor_decrypt(int(verifycode_))
+        else:
+            IS_LOGGER.error('missing params: verifycode')
+
+        if decrypt_qq == qq_:
+            try:
+                if qq_ and msg_:
+                    await bot.send_private_msg(user_id=qq_, message=msg_)
+                    rcode_ = 200
+                    rmsg_ = "发送成功"
+            except CQHttpError:
+                rcode_ = 500
+                rmsg_ = "向用户发消息失败！"
+            bot._server_app.config['JSONIFY_MIMETYPE'] = "text/html"
+            return jsonify(code=rcode_, data=None, msg=rmsg_)
+        else:
+            rmsg_ = "参数有误"
+
     return jsonify(code=rcode_, data=None, msg=rmsg_)
