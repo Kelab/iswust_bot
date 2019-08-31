72238ed00c834422a0924cb6c5a568ba80d04fd9
Artin <lengthmin@gmail.com>
2019-08-31 12:55:24 +0800

    feat: add parse grade

---
+++ iswust/plugins/grade/parse_grade.py
@@ -0,0 +1,22 @@
+import json
+
+
+def parse_grade(grade_json, info_json, type):
+    grade_msg = ''
+    info = json.loads(info_json['body']['result'])
+    gpa_msg = '总成绩绩点：' + str(info['gpa']['all']) + '\n必修课绩点：' + str(
+        info['gpa']['required'])
+    credit_msg = '总成绩学分：' + str(info['credit']['all']) + '\n必修课学分：' + str(
+        info['credit']['required'])
+
+    if type == '1':
+        term_dict = {}
+        grade = json.loads(grade_json['body']['result'])
+        for x in grade:
+            term = str(x['term'])
+            if not term_dict.get(term):
+                term_dict[term] = 1
+                grade_msg = grade_msg + '\n' + term[:-1] + '-' + term[
+                    -1] + '学期成绩如下：\n'
+            grade_msg = grade_msg + str(
+                x['catalog']) + '-' + x['course'] + '-' + x['scroll'] + '\n'
