6a2dc3b3a635682957eb14d56d0f78e175c88f3a
Artin <lengthmin@gmail.com>
2019-09-18 01:30:40 +0800

    feat: make hitokoto a command

--- app/bot/plugins/hitokoto/__init__.py
+++ app/bot/plugins/hitokoto/__init__.py
@@ -1,20 +1,31 @@
-from nonebot import (NLPSession, on_natural_language)
+from nonebot import (NLPSession, IntentCommand, on_natural_language)
 
 from typing import Optional
 
 from utils.aio import requests
 from requests import Response
+from nonebot import CommandSession, on_command
+
+__plugin_name__ = '更新课表'
+__plugin_usage__ = r"""输入 更新课表或者uc
+""".strip()
 
 
-@on_natural_language()
-async def _(session: NLPSession):
-    await session.send(f"我不知道该说些什么啦~")
+@on_command('hitokoto', aliases=('一言'))
+async def _(session: CommandSession):
     _hitokoto = await hitokoto()
     if _hitokoto:
         resp = f"{_hitokoto['from']}:\n{_hitokoto['hitokoto']}"
         await session.finish(resp)
         return
+
     await session.finish("那就祝你早安，午安，晚安吧")
+
+
+@on_natural_language()
+async def _(session: NLPSession):
+    await session.send(f"我不知道该说些什么啦~")
+    return IntentCommand(90.0, 'hitokoto')
 
 
 async def hitokoto() -> Optional[dict]:
