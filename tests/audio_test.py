2c2eb69602c4a5e63d984ef52ad40325dbc917cc
Artin <lengthmin@gmail.com>
2019-10-24 22:37:35 +0800

    test: 修正测试样例

--- tests/audio_test.py
+++ tests/audio_test.py
@@ -13,5 +13,7 @@ filename = "777D935D643B0300777D935D643B0300.silk"
 
 @pytest.mark.asyncio
 async def test_app():
-    text = await rec_silk(filename, Path("./tests/assets/record").resolve())
+    result, text = await rec_silk(filename,
+                                  Path("./tests/assets/record").resolve())
+    assert result is True
     assert text == "明天课表"
